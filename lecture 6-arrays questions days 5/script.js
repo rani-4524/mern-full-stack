b5c2b2be1ccb3a6baea139daa7b4a2825d71e357
rani-4524 <raniagalave@mail.com>
2025-09-05 05:27:49 +0100

    your commit message

--- lecture 6-arrays questions days 5/script.js
+++ lecture 6-arrays questions days 5/script.js
@@ -1,50 +1,70 @@
-//function to print sum of all subarray
+/* Function to print the sum of all subarrays */
+const arr = [1, 2, 3, 4];
 
-const arr=[1,2,3,4];
-function subArray(arr){
-    let sum=0;
-    let n =arr.length;
+function subarraySum (arr) {
+  let sum = 0;
+  let n = arr.length;
 
-    //loop for subarray starting from different 
+  //loop for subarrays starting from different index
+  for(let i = 0; i < n; i++) {
 
-    for(let i=0;i<n;i++){
-        //for each j, it will represent a subarray starting from i and ending at j
-
-        let subSum=0;
-        for(let j=i;j<n;j++){
-            subSum+=arr[j];
-            sum+=subSum;
-        }
+    // for each j, it will represent a subarray starting from i and ending at j
+    let subSum = 0;
+    for(let j = i; j< n; j++){
+      subSum += arr[j];
+      sum += subSum;
     }
-    return sum;
+  }
+
+  return sum;
 }
 
-console.log("Sum", arr, "is", subArray(arr));
+console.log("Sum of all subarrays of ", arr, "is", subarraySum(arr));
 
-// Q. delete the last element of the array
-//Using slice () method 
+/* ================== DELETIONS IN ARRAY */
+/* Ques: delete the last element of the array */
 
-let array=[1,2,3,4,5,6];
-array=array.slice(0,array.length);
-console.log(array);//using slice method doesn't change the array, until assigned to array itself
+//Using slice() -> doesn't change the array, until assigned to array itself
+let arr1 = [1,2,3,4,5,6];
+arr1 = arr1.slice(0, arr1.length - 1);
+console.log({ arr1 });
 
-//using splice () method --> changes the original array 
-//syntax for splice method--> splice(start,count)
+//Using splice() -> changes the original array
+//syntax -> splice(start, count);
+arr1.splice(arr1.length - 1, 1);
+console.log(arr1);
 
-array.splice(array.length-1,1);
-console.log(array);
+//using pop()
 
-//By changing the length of an array 
+//By changing the length of the array
+arr1.length = arr1.length - 1;
+console.log("Array after decreasing the value of length property", arr1);
 
-array.length=array.length-1;
-console.log(array);
+/* Question: Delete element from begining */
+const arr2 = [10, 20, 30, 40];
+console.log("Array before deletion of first element", arr2);
+let z = arr2.shift();
+console.log("Array after deletion of first element", arr2);
+console.log({z});
 
-//using loop 
-//just shift all the elements left by one index, and decrease the length property vlaue by 1
-
-for(let i=array.length-1;i>0;i--){
-    array[i-1]=arr[i-1];
+//Using loops
+/* just shift all the elements left by one index, and decrease the length property value by 1 */
+const arr3 = [1,2,3,4,5];
+console.log("Array before deletion of first element", arr3);
+for(let i = 0; i < arr3.length - 1; i++){
+  arr3[i] = arr3[i+1];
 }
+arr3.length = arr3.length - 1;
 
-array.length=array.length-1;
-console.log(array);
+console.log("Array after deletion of first element", arr3);
+
+/* HOMEWORK */
+/* Do each deletion question, and instead of deletion, do insertion */
+
+/* ===================== ROTATIONS in Array  ========================*/
+/* Q: Rotate array right (clockwise) by 1 element */ 
+/* Q: Rotate array left (anti - clockwise) by 1 element */ 
+
+/* HOMEWORK */
+/* Q: Write a function to Rotate array right (clockwise) by k element */ 
+/* Q: Write a function to Rotate array left (anti - clockwise) by k element */ 
